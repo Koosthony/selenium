4a4f74e23d1403003e93370d9411c73c94d9d3aa
KoosVeldman <135098473+Koosthony@users.noreply.github.com>
2024-08-30 13:30:40 +0200

    NEW NOTIFICATION

--- seleniumTraining/src/test/java/uI/demoAutomationTest.java
+++ seleniumTraining/src/test/java/uI/demoAutomationTest.java
@@ -1,21 +1,39 @@
 package uI;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class demoAutomationTest {
-	public static void main(String[] args) throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
+	// THESE FIRST STEP IS WHERE YOU WILL BE CHANGING FOR THE BROWSERE
 
-		ChromeDriver driver = new ChromeDriver();
+	public static String browser = "chrome"; // external config - Xls, CSV
+	public static WebDriver driver; // available globally
+
+	public static void main(String[] args) throws InterruptedException {
+
+ 		if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+
+		} else if (browser.equals("firfox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		// AUTOMATION SUITE PART.
 		driver.get("https://www.ebay.com/");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("mobile phones");
 		driver.findElement(By.xpath("//input[@id='gh-btn']")).click();
-		
+
 		driver.close();
 	}
 }
-
